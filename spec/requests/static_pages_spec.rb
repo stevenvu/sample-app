1e36d3337884b1c26418f4706c3d0558dbfb28a8
Steven Vu <stevenvu@gmail.com>
2014-04-21 16:50:47 +0100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,20 @@ describe "Static pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
-		end 
+		end
+		it "should have the right title" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+		end
+		it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 	describe "About page" do
@@ -17,5 +25,11 @@ describe "Static pages" do
 	    visit '/static_pages/about'
 	    expect(page).to have_content('About Us')
 		end
+		it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
 	end
+
+
 end
